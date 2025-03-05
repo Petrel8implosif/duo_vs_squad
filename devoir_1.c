7e83365065fa8c5b74773688820947605618a42d
Jérôme <jejedresse@gmail.com>
2025-03-05 17:05:24 +0100

    le début d'une grande aventure

---
+++ devoir_1.c
@@ -0,0 +1,17 @@
+#include "devoir_1.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cblas.h>
+
+void tridiagonalize_[format](double *A, int n, int k, double *d, double *e){
+
+}
+
+int step_qr_tridiag(double *d, double *e, double m, double eps){
+
+}
+
+int qr_eigs_[format](double *A, int n, int k, double eps, int max_iter){
+    
+}
